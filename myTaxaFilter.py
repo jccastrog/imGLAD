57663e88297b52730997d5c2788796ff3cfa6842
jccastrog <jccastrogo@gmail.com>
2018-06-21 15:04:15 -0400

    subprocess added

--- myTaxaFilter.py
+++ myTaxaFilter.py
@@ -24,7 +24,7 @@ except:
 parser = argparse.ArgumentParser(description="myTaxaFilter.py: Use MyTaxa to filter un-specific regions of the target genome. Before running myTaxaFilter you should install MyTaxa (https://github.com/luo-chengwei/MyTaxa) as well as download the AllGenome database in diamond format (http://enve-omics.ce.gatech.edu/data/public_mytaxa/AllGenomes.faa.dmnd) [jccastrog@gatech.edu]")
 group = parser.add_argument_group('Required arguments')
 group.add_argument('-t', action='store', dest='target', required=True, help='The target genome to in FASTA format.')
-group.add_argument('-mt', action='store', dest='my_Taxa', required=True, help = 'Path to the MyTaxa binaries.')
+group.add_argument('-mt', action='store', dest='my_taxa', required=True, help = 'Path to the MyTaxa binaries.')
 group.add_argument('-mdb', action='store', dest='db', required=False, help='Path to the MyTaxa protein database.')
 group = parser.add_argument_group('Alignment arguments')
 group.add_argument('-p', action='store', dest='threads', required=False, default='1', help='Number of threads. (default : %(default)s)')
@@ -41,8 +41,8 @@ vals = []
 path = os.path.dirname(os.path.realpath(__file__))
 #=======================1.3 Define functions=========================
 def genome_fragments(genomeFile,size,outFile):
+	fragsFile = open(outFile,'w')
 	with open(genomeFile) as fastaFile:
-		fragsFile = open(outFile,'w')
 		for fastaParse in SeqIO.parse(fastaFile,"fasta"):
 			ID = fastaParse.id
 			seq = fastaParse.seq
@@ -50,58 +50,74 @@ def genome_fragments(genomeFile,size,outFile):
 			numFrags = int(seqLen/size)
 			for i in range(1,numFrags+2):
 				fragSeq = seq[size*(i-1):fragmentLength*i]
-				fragsFile.write('>'+str(ID)+'-'+str(i)+'\n')
+				fragsFile.write(">{}\n".format(i))
 				fragsFile.write(str(fragSeq)+'\n')
 	fragsFile.close()
-def remove_regions(genomeFile,regionsFile,outFile):
+#def 
+def remove_regions(fragsFile,regionsFile,outFile):
+	finalSeq = ""
+	regsArr = []
+	with open(regionsFile) as regs:
+		lines = regs.readlines()
+		for line in lines:
+			line = line.rstrip('\n')
+			regsArr.append('line')
+	with open(fragsFile) as fastaFile:
+		for fastaParse in SeqIO.parse(fastaFile):
+				ID = fastaParse.id
+				seq = fastaParse.seq
+				if ID in regsArr:
+					continue
+				else:
+					finalSeq = finalSeq+seq
 	updatedGenome = open(outFile,'w')
-	with open(genomeFile) as fastaFile:
-		for fastaParse in SeqIO.parse(fastaFile:
-				ID
-
+	updatedGenome.write(">updatedGenome\n")
+	updatedGenome.write(finalSeq+'\n')
+	updatedGenome.close()
+	os.remove(fragsFile)
+	os.remove(regionsFile)
 
 '''2.0 Align the genome segments to the protein databaseDownload the gneomes from NCBI'''
 #=======================2.1 Create fragments from the genome file========================
 genome_fragments(args.target,fragmentLength,fragsName)
 #==============2.2 Align the fragments to the MyTaxa database with diamond===============
-os.system("diamond blastx -q "+args.target+" -d "+args.db+" -p "+args.threads+" --out "+alignName+" -f 6")
+subprocess.call(["diamond", "blastx", "-q", args.target, "-d ", args.db, "-p", args.threads, "--out", alignName, "-f", "6"])
 #================2.3 Format the allignment table with BlastTab.catsbj.pl=================
-os.system(path+"BlastTab.metaxaPrep.pl -q -f no genes.txt "+alignName+" > "+myTaxaName)
+myTaxaFile = open(myTaxaName, 'w')
+prepScript = args.my_taxa+"/BlastTab.metaxaPrep.pl"
+subprocess.call([prepScript, "-q", "-f", "no genes.txt", alignName], stdout=myTaxaFile, stderr=FNULL)
 os.remove(alignName)
-
 '''3.0 Run myTaxa and select the regions to be removed'''
 #=====================3.1 Run MyTaxa=====================
-os.syystem(args.my_taxa+"/MyTaxa "+myTaxaName+" "+outName+" 5") 
-
+myTaxaBin = args.my_taxa+"/MyTaxa "
+subprocess.call(myTaxaBin, myTaxaName, outName, "5") 
 '''4.0 Remove the regions selected from the original genome'''
 #============4.1 Check the values for each region=============
+vals = []
 with open(outName) as myTaxaFile:
 	lines = myTaxaFile.readlines()
 	for line in lines:
-		if line.startswith('<'):
+		line = line.rstrip('\n')
+		if line.startswith('>'):
 			continue
 		else:
 			fields = line.split('\t')
 			if fields[2]=='NA':
-				vals.append('NaN'
-			vals.append(fields[2])
-#=======4.2 Select the bottom 5 pecentile and remove it=======
+				vals.append('NaN')
+			else:
+				vals.append(fields[2])
+os.remove(myTaxaFile)
 valsDist = np.array(vals)
 removeTh = np.percentile(valsDist,5)
-regFile = open(regName,'w')
-with open(outName) as myTaxaFile:
-	lines = myTaxaFIle.readlines()
-	for line in lines:
-		if line.startswith('<'):
-			continue
-		else:
-			fields = line.split('\t')
-			region = fields[0]
-			value = fields[2]
-			if value<=removeTh:
-				write.regFile(region)
-
-refFile.close()
-
-with open(args.target) as fastaFile:
-	
+#=======4.2 Select the bottom 5 pecentile and remove it=======
+regFile = open(regNameFile,'w')
+lnNumber = 0
+for i in range(0,len(vals)):
+	lnNumber += 1
+	if vals[i] <= removeTh:
+		continue
+	else :
+		regFile.write("{}\n".format(i))
+regFile.close()
+remove_regions(fragsName, regNameFile, 'updatedGenome.fa')
+#=============================================================
